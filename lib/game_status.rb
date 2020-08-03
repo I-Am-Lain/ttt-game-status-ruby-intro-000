ab59b7c646feff19c99fa32edb2824eb7dfae63b
Will Vetter <vetter.william@gmail.com>
2020-08-03 09:42:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,10 +29,9 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination # return the win_combination indexes that won.
-    elsif board.all? do |space|
-      space == " "
-    end
-      false
+    elsif board.all?{|space| space == " "}
+      return false
+      
     end
   end
 end
