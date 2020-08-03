44da2ab38bda60ec3286050a8cda2128de87ad04
Will Vetter <vetter.william@gmail.com>
2020-08-03 09:40:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,7 +31,7 @@ def won?(board)
       return win_combination # return the win_combination indexes that won.
     elsif board.all? do |space|
       space == " "
-    end
+    end return false
       false
     end
   end
